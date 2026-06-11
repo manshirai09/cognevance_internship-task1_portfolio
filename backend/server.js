07f7f2fdf3df7921b70d8d1ce6926b2c312ce48d
Manshi Rai <raiisahab009@gmail.com>
2026-06-12 04:45:29 +0530

    update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const contactRoutes = require("./routes/contactRoutes");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", "https://portfolio-frontend-peach-psi.vercel.app",
+  origin:"https://portfolio-frontend-peach-psi.vercel.app",
   methods: ["GET", "POST"],
 }));
 app.use(express.json());
